dd25c0d25900ddef0130fb78c94c1b666a48e100
kaka cool <66282619+mahendrajungthapa@users.noreply.github.com>
2024-04-05 16:31:21 +0545

    Update fonepay.php

--- fonepay.php
+++ fonepay.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: Fonepay Payment Gateway Nepal
+ * Plugin Name: Fonepay Payment Nepal
  * Plugin URI: https://github.com/mahendrajungthapa/Fonepay
  * Description: Accept payments via Fonepay .
  * Author: Mahendra Jung Thapa
