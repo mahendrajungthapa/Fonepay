1341a39beda5c77754d5fb4796ee5cd903a49e51
kaka cool <66282619+mahendrajungthapa@users.noreply.github.com>
2024-04-05 16:33:19 +0545

    Update fonepay.php

--- fonepay.php
+++ fonepay.php
@@ -5,7 +5,7 @@
  * Description: Accept payments via Fonepay .
  * Author: Mahendra Jung Thapa
  * Author URI: http://mahendrathapa.com.np/
- * Version: 1.3.0
+ * Version: 1.0.0
  * License: GPLv2 or later
  * License URI: http://www.gnu.org/licenses/gpl-2.0.html
  */
